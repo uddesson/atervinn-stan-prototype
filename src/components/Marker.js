795705cbd6a9252bf729918cac670e747b080e15
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-06-07 09:54:02 +0200

    initial styling for map markers

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import RecyclingIcon from '../icons/icons8-recycling-80.png';
+import GrillIcon from '../icons/icons8-grill-80.png';
 
 function Marker(props) {
     return (
         <div
-            style={{ height: '10px', width: '10px', backgroundColor: 'red' }}
+            style={{
+                width: '30px',
+                height: '30px',
+                backgroundColor: '#F8FAFC',
+                display: 'flex',
+                justifyContent: 'center',
+                borderRadius: '0.25rem',
+            }}
             lat={props.lat}
             lng={props.lng}
         >
-            {props.text}
+            <img
+                src={
+                    props.type === 'allmän återvinning'
+                        ? RecyclingIcon
+                        : GrillIcon
+                }
+                alt={
+                    props.type === 'allmän återvinning'
+                        ? 'allmän återvinning'
+                        : 'återvinning för engångsgrill'
+                }
+                style={{ height: '20px', width: '20px', alignSelf: 'center' }}
+            />
         </div>
     );
 }
