b319418f960c02eb60f7c519d29f9314e4c4bf06
Emma Uddesson <emma.uddesson@gmail.com>
2018-06-07 13:50:50 +0200

    Remove unused lines in SearchInput-component

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -4,24 +4,18 @@ class SearchInput extends Component {
 
     render(){
         return(
-            <React.Fragment>
-                <form onSubmit={this.props.handleSearch}>
-                    <label htmlFor="search" className="hidden">Vad vill du återvinna?</label>
-                    <br/>
-                    <input  name="search"
-                            type="text"
-                            placeholder="T.ex plastbestick"
-                            onChange={this.props.setSeachWordToState}
-                            value={this.props.searchWord}
-                    >
-                    </input>
-                    <input type="submit" value="Sök"></input>
-                </form>
-
-                <div>
-                    {/* XX kan källsorteras som plast på en av våra källsorteringsmoduler. */}
-                </div>
-            </React.Fragment>
+            <form onSubmit={this.props.handleSearch}>
+                <label htmlFor="search" className="hidden">Vad vill du återvinna?</label>
+                <br/>
+                <input  name="search"
+                        type="text"
+                        placeholder="T.ex plastbestick"
+                        onChange={this.props.setSeachWordToState}
+                        value={this.props.searchWord}
+                >
+                </input>
+                <input type="submit" value="Sök"></input>
+            </form>
         )
     }
 }
