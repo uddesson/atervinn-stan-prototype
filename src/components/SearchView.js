91609d987af3bb15f22d8bc11aa4007547828def
Emma Uddesson <emma.uddesson@gmail.com>
2018-06-07 21:12:04 +0200

    Improve SearchView

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchInput from './SearchInput';
 import Button from './Button';
 import searchData from '../searchData.json';
+import miscIcon from '../icons/icons8-disposal-80.png';
 
 class SearchView extends React.Component {
 
@@ -26,7 +27,10 @@ class SearchView extends React.Component {
 
         if(matches.length === 0) {
             this.setState({
-                searchOutput: { noResults: 'Vi hittar inga resultat som matchar din sökning.'},
+                searchOutput: {
+                    name: '',
+                    noResults: 'Vi hittar inga resultat som matchar din sökning. Testa något annat!'
+                },
                 showActionButton: false
             })
         }
@@ -36,16 +40,16 @@ class SearchView extends React.Component {
                 if(result.compatible === false){
                     return {
                         name: result.name,
-                        sortedAs: result.sortedAs,
-                        text: ' kan du inte återvinna i stan. Sorteras på en återvinningscentral som ',
+                        sortedAs: result.sortedAs + '.',
+                        text: ' kan inte återvinnas i stan. Sorteras på en återvinningscentral som ',
                         compatible: result.compatible
                     }
                 }
                 else if(result.compatible === true){
                     return {
                         name: result.name,
-                        sortedAs: result.sortedAs,
-                        text: ' kan du återvinna i stan. Sorteras som ',
+                        sortedAs: result.sortedAs + '.',
+                        text: ' kan återvinnas i stan. Sorteras som ',
                         compatible: result.compatible
                     }
                 }
@@ -64,10 +68,10 @@ class SearchView extends React.Component {
         }
     }
 
-
     render(){
         return (
-            <div className="container center">
+            <div className="container center line-height-extra">
+
                 <h2>Vad vill du återvinna?</h2>
 
                 <SearchInput
@@ -76,22 +80,33 @@ class SearchView extends React.Component {
                     handleSearch={this.handleSearch}
                 />
 
-                
-                <p>
-                    <span className="red">{this.state.searchOutput.name}</span>
-                    {this.state.searchOutput.text}
-                    <span className="green">{this.state.searchOutput.sortedAs}</span>
-                    <span className="pink">{this.state.searchOutput.noResults}</span>
-                </p>
+                <div className="container-inner">
+                    <p>
+                        {this.state.searchOutput.name !== '' &&
+                            <span className="outlined capitalize">
+                                {this.state.searchOutput.name}
+                            </span>
+                        }
 
-                {this.state.showActionButton &&
-                    <Button
-                        onClick={this.props.toggleView}
-                        value={'karta'}
-                        content={'Hitta närmsta'}
-                        className={'btn__call-to-action'}
-                    />
-                }
+                        {this.state.searchOutput.text}
+
+                        <span className="success">{this.state.searchOutput.sortedAs}</span>
+                        <span className="warning">{this.state.searchOutput.noResults}</span>
+
+                        {this.state.searchOutput.noResults &&
+                            <img src={miscIcon} alt="Papperskorg" className="margin-y-5"></img>
+                        }
+                    </p>
+
+                    {this.state.showActionButton &&
+                        <Button
+                            onClick={this.props.toggleView}
+                            value={'karta'}
+                            content={'Hitta närmsta ställe'}
+                            className={'btn__call-to-action'}
+                        />
+                    }
+                </div>
             </div>
         )
     }
