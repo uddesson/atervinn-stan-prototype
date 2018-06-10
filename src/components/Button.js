9c92c60423695b01f0bd1e1ba046e6f41256fef3
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-06-10 13:21:39 +0200

    nav styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 function Button(props) {
-    return(
+    return (
         <button
             className={props.className}
             onClick={props.onClick}
             value={props.value}
+            type={props.type}
         >
-            {props.content}
+            {props.type === 'nav' ? <img src={props.icon} alt=" " className="icon--small" /> : null}
+            <p className={props.type === 'nav' ? 'margin-small' : ''}>{props.content}</p>
         </button>
-    )
+    );
 }
 
 export default Button;
