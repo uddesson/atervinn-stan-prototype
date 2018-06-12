cfa6d4aec3ec57f97f189d7d8f7496a2c6626a73
Emma Uddesson <emma.uddesson@gmail.com>
2018-06-12 21:42:41 +0200

    Update styling of Map-component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -40,7 +40,7 @@ class GoogleMap extends Component {
 
         return (
             // Important! Always set the container height explicitly
-            <div style={{ height: '100vh', width: '100%' }}>
+            <div style={{ height: '88vh', width: '100%' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{
                         key: 'AIzaSyCT5AuY2Mf3nQ_YgE7LXakaBT_KQEDDa6U',
