3142833950ee029b1410f04ec5e4446b75c11622
Emma Uddesson <emma.uddesson@gmail.com>
2018-06-12 22:46:33 +0200

    More additional styles for Map components

--- src/components/CurrentMarker.js
+++ src/components/CurrentMarker.js
@@ -3,8 +3,8 @@ import React from 'react';
 function CurrentMarker(props) {
     return (
         <div lat={props.lat} lng={props.lng}>
-            <div className="pin" />
-            <div className="pulse" />
+            <div className="pin-colorful" />
+            <div className="pulse-colorful" />
         </div>
     );
 }
