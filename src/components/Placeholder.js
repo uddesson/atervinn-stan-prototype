76ef404ac14dfc9a5ee59e3165e575e097745e11
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-08-27 14:45:14 +0200

    fallback css for desktop

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -4,13 +4,14 @@ import HeartIcon from '../icons/basic_elaboration_message_heart.svg';
 function Placeholder(props) {
     return (
         <React.Fragment>
-            Osäker på var eller hur ditt skräp ska återvinnas?
-            Använd sökfunktionen. Du kan också gå direkt till
-                <span class="link" data-txt="karta" onClick={props.toggleView}>
-                    <span> </span>närmsta station.
-                </span>
+            Osäker på var eller hur ditt skräp ska återvinnas? Använd
+            sökfunktionen. Du kan också gå direkt till
+            <span class="link" data-txt="karta" onClick={props.toggleView}>
+                <span> </span>
+                närmsta station.
+            </span>
             <img
-                className="margin-top-large center block"
+                className="margin-top-large center block heart-message-icon"
                 src={HeartIcon}
                 alt="Ett hjärta i en pratbubbla"
             />
